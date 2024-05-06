39dadf7ff668c12b8cdd0e10c4715e9b8a3ff29a
mariam mahmoud <mariamsayed777@gmail.com>
2024-05-06 04:34:08 +0200

    Add hidden gems in db

---
+++ demo/src/main/java/com/example/demo/controllers/LocalBusinessOwnerController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.models.HiddenGem;
+import com.example.demo.repositories.HiddenGemRepository;
+
+@RestController
+@RequestMapping("/LocalBusinessOwner")
+public class LocalBusinessOwnerController {
+    @Autowired
+    private HiddenGemRepository hiddenGemRepository;
+    
+    @GetMapping("addHiddenGem")
+    public ModelAndView addHiddenGems() {
+        ModelAndView mav = new ModelAndView("/localBusinessOwner/addHiddenGem.html");
+        HiddenGem newHiddenGem = new HiddenGem();
+        mav.addObject("hiddenGem", newHiddenGem);
+        return mav;
+    }
+
+    @PostMapping("addHiddenGem")
+    public ModelAndView saveHiddenGem(@ModelAttribute HiddenGem hiddenGem) {
+
+        this.hiddenGemRepository.save(hiddenGem);
+        return new ModelAndView("redirect:/LocalBusinessOwner/addHiddenGem");
+    }
+}
