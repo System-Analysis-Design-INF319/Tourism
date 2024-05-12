fb094b277211209391b6677bc4495ad506cc8604
janaelmorshedy <janaelmorshedy20@gmail.com>
2024-05-12 20:31:14 +0300

    transportation repository

---
+++ demo/src/main/java/com/example/demo/repositories/TransportationRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+import com.example.demo.models.Transportation;
+
+public interface TransportationRepository extends JpaRepository<Transportation, Integer> {
+   
+
+   
+}
