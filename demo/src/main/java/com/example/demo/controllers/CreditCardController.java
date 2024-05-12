ca39e16ef5f6df78ab88d7a2b77098791598ccee
janaelmorshedy <janaelmorshedy20@gmail.com>
2024-05-12 20:31:25 +0300

    creditcard controller

--- demo/src/main/java/com/example/demo/controllers/CreditCardController.java
+++ demo/src/main/java/com/example/demo/controllers/CreditCardController.java
@@ -1,67 +1,67 @@
-package com.example.demo.controllers;
+// package com.example.demo.controllers;
 
-import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-import com.example.demo.models.CreditCard;
+// import org.springframework.validation.Errors;
+// import org.springframework.validation.annotation.Validated;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.servlet.ModelAndView;
+// import com.example.demo.models.CreditCard;
 
 
-@RestController
+// @RestController
 
-@RequestMapping("/")
-public class CreditCardController {
+// @RequestMapping("/")
+// public class CreditCardController {
   
     
-    @GetMapping("/validate")
+//     @GetMapping("/validate")
     
-    public ModelAndView addCreditCard() {         
-    ModelAndView mav = new ModelAndView("/tourist/creditcard.html");
-        CreditCard newCreditCard = new CreditCard();
-        mav.addObject("creditcard", newCreditCard);
-        return mav;
-    }
-    @PostMapping("/validate")
-        public ModelAndView savecreditcard(@Validated @ModelAttribute CreditCard creditcard) {
-            ModelAndView mav = new ModelAndView("/tourist/creditcard.html");
-            mav.addObject("creditcard", creditcard);
+//     public ModelAndView addCreditCard() {         
+//     ModelAndView mav = new ModelAndView("/tourist/creditcard.html");
+//         CreditCard newCreditCard = new CreditCard();
+//         mav.addObject("creditcard", newCreditCard);
+//         return mav;
+//     }
+//     @PostMapping("/validate")
+//         public ModelAndView savecreditcard(@Validated @ModelAttribute CreditCard creditcard) {
+//             ModelAndView mav = new ModelAndView("/tourist/creditcard.html");
+//             mav.addObject("creditcard", creditcard);
 
-            if (creditcard.isEmpty(creditcard.getCardNumber()) || creditcard.isEmpty(creditcard.getCvv()) ||
-            creditcard.isEmpty(creditcard.getExpiryDate())) {
-            mav.addObject("emptyFieldsError", "Please fill in all fields");
-            mav.addObject("hasEmptyFieldsError", true);
-            }
-            else {
-            if (String.valueOf(creditcard.getCvv()).length() != 3) {
-                mav.addObject("CvvError", "Cvv must be 3 digits");
-                mav.addObject("hasCvvError", true);
-            }
+//             if (creditcard.isEmpty(creditcard.getCardNumber()) || creditcard.isEmpty(creditcard.getCvv()) ||
+//             creditcard.isEmpty(creditcard.getExpiryDate())) {
+//             mav.addObject("emptyFieldsError", "Please fill in all fields");
+//             mav.addObject("hasEmptyFieldsError", true);
+//             }
+//             else {
+//             if (String.valueOf(creditcard.getCvv()).length() != 3) {
+//                 mav.addObject("CvvError", "Cvv must be 3 digits");
+//                 mav.addObject("hasCvvError", true);
+//             }
             
 
-            if (String.valueOf(creditcard.getCardNumber()).length() != 15 ) {
-                mav.addObject("CardNumberError", "CardNumber must be 15 digits");
-                mav.addObject("hasCardNumberError", true);
-            }
-            if (mav.getModel().containsKey("hasCvvError") ||
-            mav.getModel().containsKey("hasCardNumberError") )          
-            return mav;
+//             if (String.valueOf(creditcard.getCardNumber()).length() != 15 ) {
+//                 mav.addObject("CardNumberError", "CardNumber must be 15 digits");
+//                 mav.addObject("hasCardNumberError", true);
+//             }
+//             if (mav.getModel().containsKey("hasCvvError") ||
+//             mav.getModel().containsKey("hasCardNumberError") )          
+//             return mav;
 
-            // return new ModelAndView("redirect:/tourist/index");
+//             // return new ModelAndView("redirect:/tourist/index");
            
-        }
+//         }
         
-            return mav;
-    }
-}
+//             return mav;
+//     }
+// }
 
 
 
 
-// CardNumber;
-// private int Cvv;
-// private int ExpiryDate;
+// // CardNumber;
+// // private int Cvv;
+// // private int ExpiryDate;
 
