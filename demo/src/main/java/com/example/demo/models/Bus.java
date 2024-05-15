b9eb9de3060d7992ae513e6424ecff31bd3c5bfb
SaraYasser-03 <108815286+SaraYasser-03@users.noreply.github.com>
2024-05-16 01:06:47 +0300

    bus booking

--- demo/src/main/java/com/example/demo/models/Bus.java
+++ demo/src/main/java/com/example/demo/models/Bus.java
@@ -5,7 +5,7 @@ import java.time.LocalTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.Id; 
 import java.util.Objects;
 
 @Entity
