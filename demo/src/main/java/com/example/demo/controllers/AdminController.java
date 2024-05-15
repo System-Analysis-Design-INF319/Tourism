33b8cb8070cadb6d223a7e0359c1a45b5ff99e5b
SaraaMostafaa <125473437+SaraaMostafaa@users.noreply.github.com>
2024-05-16 01:08:35 +0300

    edit Business Owner

--- demo/src/main/java/com/example/demo/controllers/AdminController.java
+++ demo/src/main/java/com/example/demo/controllers/AdminController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.models.LocalBusinessOwner;
@@ -35,6 +37,21 @@ public class AdminController {
     public ModelAndView addBusinessOwner(@ModelAttribute LocalBusinessOwner businessOwner) {
         localBusinessOwnerRepository.save(businessOwner);
         return new ModelAndView("redirect:/admin/businessOwners"); 
+    }
+
+    @GetMapping("/editBusinessOwner/{id}")
+    public ModelAndView editBusinessOwnerForm(@PathVariable Long id) {
+        LocalBusinessOwner businessOwner = localBusinessOwnerRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid business owner Id:" + id));
+        ModelAndView mav = new ModelAndView("admin/editBusinessOwner");
+        mav.addObject("businessOwner", businessOwner);
+        return mav;
+    }
+
+    @PostMapping("/editBusinessOwner")
+    public ModelAndView editBusinessOwner(@ModelAttribute LocalBusinessOwner businessOwner) {
+    localBusinessOwnerRepository.save(businessOwner);
+    return new ModelAndView("redirect:/admin/businessOwners");
     }
 
 }
