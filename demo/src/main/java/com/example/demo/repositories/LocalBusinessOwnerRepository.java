d1d286921358153e7b973d3bafdd5c4fbf2d7fd1
SaraYasser-03 <108815286+SaraYasser-03@users.noreply.github.com>
2024-05-16 01:10:40 +0300

    solving error

--- demo/src/main/java/com/example/demo/repositories/LocalBusinessOwnerRepository.java
+++ demo/src/main/java/com/example/demo/repositories/LocalBusinessOwnerRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.repositories;
 import com.example.demo.models.LocalBusinessOwner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LocalBusinessOwnerRepository extends JpaRepository<LocalBusinessOwner, Long> {
+public interface LocalBusinessOwnerRepository extends JpaRepository<LocalBusinessOwner, Integer> {
     
 }
