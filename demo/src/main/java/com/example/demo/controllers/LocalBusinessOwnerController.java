d54e988a4054acda9fc8e364762817a8b1e764ac
mariam mahmoud <mariamsayed777@gmail.com>
2024-05-17 16:47:15 +0200

    view hidden gem info

--- demo/src/main/java/com/example/demo/controllers/LocalBusinessOwnerController.java
+++ demo/src/main/java/com/example/demo/controllers/LocalBusinessOwnerController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controllers;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +33,14 @@ public class LocalBusinessOwnerController {
     public ModelAndView saveHiddenGem(@ModelAttribute HiddenGem hiddenGem) {
         this.hiddenGemRepository.save(hiddenGem);
         return new ModelAndView("redirect:/LocalBusinessOwner/addHiddenGem");
+    }
+
+    @GetMapping("hiddenGemInfo")
+    public ModelAndView getHiddenGemInfo() {         
+        ModelAndView mav = new ModelAndView("/localBusinessOwner/hiddenGemInfo.html");
+        List<HiddenGem> hiddenGems = this.hiddenGemRepository.findAll();
+        mav.addObject("hiddenGems", hiddenGems);
+        return mav;
     }
 
 }
