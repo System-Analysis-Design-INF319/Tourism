71d39d51e9c78ed0ce4e9ef2db061b4a2822c3b0
SaraaMostafaa <125473437+SaraaMostafaa@users.noreply.github.com>
2024-05-22 21:37:58 +0300

    Admin model

---
+++ demo/src/main/java/com/example/demo/models/Admin.java
@@ -0,0 +1,104 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Admin {
+    @Id
+    private int id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+
+    public Admin() {
+    }
+
+    public Admin(int id, String name, String email, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Admin id(int id) {
+        setId(id);
+        return this;
+    }
+
+    public Admin name(String name) {
+        setName(name);
+        return this;
+    }
+
+    public Admin email(String email) {
+        setEmail(email);
+        return this;
+    }
+
+    public Admin phoneNumber(String phoneNumber) {
+        setPhoneNumber(phoneNumber);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Admin)) {
+            return false;
+        }
+        Admin admin = (Admin) o;
+        return id == admin.id && Objects.equals(name, admin.name) && Objects.equals(email, admin.email) && Objects.equals(phoneNumber, admin.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", email='" + getEmail() + "'" +
+            ", phoneNumber='" + getPhoneNumber() + "'" +
+            "}";
+    }
+    
+}
